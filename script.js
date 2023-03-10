6a7175a6a3822c498f118fcdf5f7a7cce7a1e6dc
TrSola <willy718066@gmail.com>
2023-03-10 20:44:04 +0800

    blurry-loading

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-undef */
 // const senior = document.querySelector('.senior');
 // // const wrap = document.querySelector('.wrap')
 
-
 // senior.addEventListener('click', function(e){
-//     if((e.target.nodeName)=="SPAN"){
-//   alert("別再點了");
+//     if((e.target.nodeName)=='SPAN'){
+//   alert('別再點了');
 // }
 //   else{
 //     alert('開門啦');
@@ -22,85 +22,103 @@
 //   newWin2 = open();
 //   newWin2.location.href = mySl2.options[mySl2.selectedIndex].value;}
 
-$(".dropAn5")
-  .mouseover(function() {
-  $(".dropdownAn5").show(300);
-});
-$(".dropAn5")
-  .mouseleave(function() {
-  $(".dropdownAn5").hide(300);     
-});
+$('.dropAn5')
+  .mouseover(() => {
+    $('.dropdownAn5').show(300);
+  });
+$('.dropAn5')
+  .mouseleave(() => {
+    $('.dropdownAn5').hide(300);
+  });
 
-$(".dropShih5")
-  .mouseover(function() {
-  $(".dropdownShih5").show(300);
-});
-$(".dropShih5")
-  .mouseleave(function() {
-  $(".dropdownShih5").hide(300);     
-});
+$('.dropShih5')
+  .mouseover(() => {
+    $('.dropdownShih5').show(300);
+  });
+$('.dropShih5')
+  .mouseleave(() => {
+    $('.dropdownShih5').hide(300);
+  });
 
-$(".dropAn6")
-  .mouseover(function() {
-  $(".dropdownAn6").show(300);
-});
-$(".dropAn6")
-  .mouseleave(function() {
-  $(".dropdownAn6").hide(300);     
-});
+$('.dropAn6')
+  .mouseover(() => {
+    $('.dropdownAn6').show(300);
+  });
+$('.dropAn6')
+  .mouseleave(() => {
+    $('.dropdownAn6').hide(300);
+  });
 
-$(".dropShih6")
-  .mouseover(function() {
-  $(".dropdownShih6").show(300);
-});
-$(".dropShih6")
-  .mouseleave(function() {
-  $(".dropdownShih6").hide(300);     
-});
+$('.dropShih6')
+  .mouseover(() => {
+    $('.dropdownShih6').show(300);
+  });
+$('.dropShih6')
+  .mouseleave(() => {
+    $('.dropdownShih6').hide(300);
+  });
 
-Swal.fire({
-  title: '歡迎光臨，已進入收費模式，每分鐘十萬元，未滿一分鐘以一分鐘計算。',
-  width: 780,
-  padding: '3em',
-  color: '#716add',
-  background: '',
-  backdrop: `
-    rgba(0,0,123,0.4)
- url(https://sweetalert2.github.io/#examplesimages/trees.png)',
-    left top
-    no-repeat
-  `
-})
+// Swal.fire({
+//   title: '好的開始是經的爸爸的一半',
+//   width: 780,
+//   padding: '3em',
+//   color: '#716add',
+//   background: '',
+//   backdrop: `
+//     rgba(0,0,123,0.4)
+//  url(https://sweetalert2.github.io/#examplesimages/trees.png)',
+//     left top
+//     no-repeat
+//   `,
+// });
 
-const panels = document.querySelectorAll('.panel'); 
-
-panels.forEach( panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
-    })
-})
-
+const panels = document.querySelectorAll('.panel');
 function removeActiveClasses() {
-    panels.forEach( panel => {
-        panel.classList.remove('active');
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
 }
 
-const panelBgsizeCover = document.querySelector(".panelBgsizeCover")
-const panelBgsizeContain = document.querySelector(".panelBgsizeContain")
-const panel = document.querySelectorAll(".panel")
-panelBgsizeContain.addEventListener("click" , function() {
-  
-panel.forEach(function (item) {
-  item.style.backgroundSize = "contain"
-})
-})
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
-panelBgsizeCover.addEventListener("click" , function() {
-  
-  panel.forEach(function (item) {
-    item.style.backgroundSize = "cover"
-  })
-  })
-  
+const panelBgsizeCover = document.querySelector('.panelBgsizeCover');
+const panelBgsizeContain = document.querySelector('.panelBgsizeContain');
+const panel = document.querySelectorAll('.panel');
+panelBgsizeContain.addEventListener('click', () => {
+  panel.forEach(() => {
+    panel.style.backgroundSize = 'contain';
+  });
+});
+
+panelBgsizeCover.addEventListener('click', () => {
+  panel.forEach(() => {
+    item.style.backgroundSize = 'cover';
+  });
+});
+
+const scale = (num, inMin, inMax, outMin, outMax) => (
+  (num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+let load = 0;
+// eslint-disable-next-line no-use-before-define
+const int = setInterval(blurring, 30);
+function blurring() {
+  load += 1;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
